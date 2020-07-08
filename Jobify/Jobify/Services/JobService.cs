4555b4ea29f5ed4ad4288fd64f32f19e16220945
rjlovesu <rjlovesu@github.com>
2020-07-08 19:44:29 +0300

    added placeholders for buttons

--- Jobify/Jobify/Services/JobService.cs
+++ Jobify/Jobify/Services/JobService.cs
@@ -8,13 +8,13 @@ namespace Jobify.Services
 
         //TODO this will change
         public static List<Job> allJobs = new List<Job>{
-            new Job(new Position(56.882859, 23.957760),"Example1"),
-            new Job(new Position(57.075190, 24.316189),"Example2"),
-            new Job(new Position(56.660865, 24.222805),"Example3"),
-            new Job(new Position(56.633684, 23.772365),"Example4"),
-            new Job(new Position(57.125913, 24.761135),"Example5"),
-            new Job(new Position(56.916608, 23.319179),"Example6"),
-            new Job(new Position(56.567159, 23.720180),"Example7"),
+            new Job(new Position(56.882859, 23.957760),"JobExample1"),
+            new Job(new Position(57.075190, 24.316189),"JobExample2"),
+            new Job(new Position(56.660865, 24.222805),"JobExample3"),
+            new Job(new Position(56.633684, 23.772365),"JobExample4"),
+            new Job(new Position(57.125913, 24.761135),"JobExample5"),
+            new Job(new Position(56.916608, 23.319179),"JobExample6"),
+            new Job(new Position(56.567159, 23.720180),"JobExample7"),
         };
 
         public List<Job> GetAllJobs() {
