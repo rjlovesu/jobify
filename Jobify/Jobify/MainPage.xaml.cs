98b350a6c57805a6fbd42c494674af45ba32c15c
rjlovesu <rjlovesu@github.com>
2020-07-09 18:32:51 +0300

    added button icons and changed map style

--- Jobify/Jobify/MainPage.xaml.cs
+++ Jobify/Jobify/MainPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using Jobify.Services;
 using System;
 using System.ComponentModel;
+using System.Net;
 using System.Reflection;
+using System.Threading;
 using Xamarin.Forms;
 using Xamarin.Forms.GoogleMaps;
 //using Xamarin.Forms.Maps;
@@ -13,31 +15,62 @@ namespace Jobify {
     public partial class MainPage : ContentPage {
         public MainPage() {
             InitializeComponent();
+            SetButtons();
             StyleMap();
             UpdateMap();
         }
 
-        public void StyleMap() {
-            
+        void SetButtons() {
+            var hamburger_button = new ImageButton() {
+                Source = "hamburger.png",
+                BackgroundColor = new Color(0, 0, 0, 0)
+            };
+            RLayout.Children.Add(hamburger_button,
+                Constraint.RelativeToParent(rl => rl.Width * 0.03),
+                Constraint.RelativeToParent(rl => rl.Width * 0.03));
+
+            var filter_button = new ImageButton() {
+                Source = "suitcase.png",
+                BackgroundColor= new Color(0,0,0,0)
+            };
+            RLayout.Children.Add(filter_button,
+                Constraint.RelativeToView(hamburger_button, (rl, fb) => rl.Width - rl.Width * 0.03 - fb.Width),
+                Constraint.RelativeToParent(rl => rl.Width * 0.03));
+
+            var location_button = new ImageButton() {
+                Source = "target.png",
+                BackgroundColor = new Color(0, 0, 0, 0)
+            };
+            RLayout.Children.Add(location_button,
+                Constraint.RelativeToView(hamburger_button, (rl, lb) => rl.Width - rl.Width * 0.03 - lb.Width),
+                Constraint.RelativeToView(hamburger_button, (rl, lb) => rl.Height - rl.Width * 0.03 - lb.Height));
+
+        }
+
+
+        void StyleMap() {
+
+
             try {
                 var assembly = typeof(MainPage).GetTypeInfo().Assembly;
-                var stream = assembly.GetManifestResourceStream($"Jobify.MapStyle.json");
+                var stream = assembly.GetManifestResourceStream($"Jobify.Map.MapStyle.json");
                 Console.WriteLine(stream.ToString());
                 string styleFile;
                 using(var reader = new System.IO.StreamReader(stream)) {
                     styleFile = reader.ReadToEnd();
                 }
+
                 MainMap.MapStyle = MapStyle.FromJson(styleFile);
             } catch(Exception e) {
-                Console.WriteLine("Something wrong with MapStyle file: "+e.Message);
-                return;
-            }
-           
+                Console.WriteLine("Something wrong with MapStyle file: " + e.Message);
 
-            
+            }
+            MainMap.UiSettings.ZoomControlsEnabled = false;
+            MainMap.UiSettings.MyLocationButtonEnabled = false;
+
         }
 
-        public async void UpdateMap() {
+        async void UpdateMap() {
             //adding pins where jobs are
             MainMap.ItemsSource = ((JobService)ServiceManager.Instance.GetService(typeof(JobService))).GetAllJobs();
 
